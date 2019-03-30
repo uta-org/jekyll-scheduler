e3e9e9858812a0fc2db77d9f31bf053009fed304
Ikillnukes <ikillnukes1@gmail.com>
2019-03-30 07:17:50 +0100

    Finished

--- test.sh
+++ test.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 IFS="="
+
+schedule_date=""
+schedule_post=""
+
 while read -r name value
 do
-# echo "Content of $name is ${value//\"/}"
+
+if [[ $name == "schedule_date" ]]; then
+	schedule_date="$value"
+elif [[ $name == "schedule_post" ]]; then
+	schedule_post="$value"
+
+	# Then do the magic
+	if [[ $schedule_date == "$(date +%Y-%m-%d)" && -d ~/repo/ ]]; then
+		mv ~/repo/_posts/schedule/$schedule_post ~/repo/_posts/$schedule_post
+	elif [[ ! -d ~/repo/ ]]; then
+		echo "You have bad configuration on circle.yml ($schedule_post)"
+	fi
+fi
 
 done < "sch.ini"
